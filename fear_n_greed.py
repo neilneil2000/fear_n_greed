61b97c36c562a58e6340ccffd71cbe8519b5e62e
neilneil2000 <31366098+neilneil2000@users.noreply.github.com>
2021-10-13 18:05:19 +0100

    Increase Bouncetime to 300ms

--- fear_n_greed.py
+++ fear_n_greed.py
@@ -85,7 +85,7 @@ def swipe_down(high, low):
 #Set Up On_Off Button
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-GPIO.add_event_detect(27, GPIO.FALLING, bouncetime=200)
+GPIO.add_event_detect(27, GPIO.FALLING, bouncetime=300)
 GPIO.add_event_callback(27, callback_27)
 
 init_leds()
